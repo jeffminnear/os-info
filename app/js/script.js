b4f9d05fcdb8f9141fb282d7e83ba42d57d68751
Jeff Minnear <jeffminnear@gmail.com>
2016-05-03 12:41:51 -0400

    Display system information

---
+++ app/js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const $           = require('cheerio');
+const os          = require('os');
+const prettyBytes = require('pretty-bytes');
+
+$(function () {
+  $('.operating-system .platform').append('<span>' + os.platform() + '</span>');
+  $('.operating-system .release').append('<span>' + os.release() + '</span>');
+  $('.operating-system .arch').append('<span>' + os.arch() + '</span>');
+
+  $('.cpu .model').append('<span>' + os.cpus()[0].model + '</span>');
+  $('.cpu .cores').append('Number of CPU Cores: <span>' + os.cpus().length + '</span>');
+
+  $('.memory .free').append('Available memory: <span>' + prettyBytes(os.freemem()) + '</span>');
+  $('.memory .total').append('Total memory: <span>' + prettyBytes(os.totalmem()) + '</span>');
+});
