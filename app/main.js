e64a6113a33a858ed96eadac16ac7b077247d9b3
Jeff Minnear <jeffminnear@gmail.com>
2016-05-03 12:12:58 -0400

    Initialize window

---
+++ app/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+var mainWindow = null;
+
+// Quit when all windows are closed
+app.on('window-all-closed', function () {
+  // Except on OS X
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+// Initialize application
+app.on('ready', function () {
+  // Create browser window
+  mainWindow = new BrowserWindow({
+
+  // Load index.html
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // Emitted when window is closed
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+});
